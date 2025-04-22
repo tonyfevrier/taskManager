4cea379a8f2e7d64493ae7926e58f40839933fad
FEVRIER <tony.fevrier62@gmail.com>
2025-04-22 16:41:16 +0200

    style(fxml, css): add inputs for tasks

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+
+
+module taskManager {
+    requires javafx.controls; // Gérer les éléments de base de JavaFX (boutons, labels, etc.)
+    requires javafx.fxml;
+    requires transitive javafx.graphics;
+    
+    opens org.openjfx to javafx.fxml; // org.openjfx est le package que je crée via ce projet. opens rend toutes les classes (public et private) de ce package accessibles à javafx.fxml (pour le fxml et le css).
+    exports org.openjfx; // Rend les classes publiques du module visible à d'autres modules
+}
---
+++ src/main/java/org/openjfx/MainApp.java
@@ -0,0 +1,28 @@
+package org.openjfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader; 
+import javafx.scene.Parent;
+import javafx.scene.Scene; // Gérer le style (css) et l'interactivité de l'application (comme JS)
+import javafx.stage.Stage; // Créer la fenêtre qui a été stylisée et gérée dans stage
+import javafx.scene.control.*;
+
+public class MainApp extends Application {
+
+    @Override 
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm()); // add prend une URL absolue sous forme de str (ce que permet toExternalForm)
+        
+        stage.setTitle("JavaFX and Gradle");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args); // Method coming from Application class
+    }
+
+}
